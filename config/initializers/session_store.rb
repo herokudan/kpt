f1a37199a6c8d8adf66cc89ec572fec44a300d6c
akiraabe <akiraabe2005@yahoo.co.jp>
2011-05-14 07:14:38 +0900

    rails new kpt -J

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Kpt::Application.config.session_store :cookie_store, :key => '_kpt_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Kpt::Application.config.session_store :active_record_store
